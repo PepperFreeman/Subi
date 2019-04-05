994654812bb774db63a526de1a98e9fed70d6c6b
Dhabbah <46940721+dhabbah@users.noreply.github.com>
2019-04-05 15:10:58 -0500

    Firebase is connected to the project.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,13 +9,30 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireModule } from 'angularfire2';
+import {ReactiveFormsModule} from '@angular/forms';
+const FireBase = {
+  apiKey: 'AIzaSyAAUrjwZhnBrSnwBH5yliQ9ria0hKi3gn4',
+  authDomain: 'subi-702d3.firebaseapp.com',
+  databaseURL: 'https://subi-702d3.firebaseio.com',
+  projectId: 'subi-702d3',
+  storageBucket: 'subi-702d3.appspot.com',
+  messagingSenderId: '173385475637'
+};
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [
     BrowserModule,
     IonicModule.forRoot(),
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireDatabaseModule,
+    AngularFireAuthModule,
+    AngularFireModule.initializeApp(FireBase),
+    ReactiveFormsModule
   ],
   providers: [
     StatusBar,
--- src/karma.conf.js
+++ src/karma.conf.js
@@ -13,6 +13,7 @@ module.exports = function (config) {
       require('@angular-devkit/build-angular/plugins/karma')
     ],
     files: [
+      'js/*.js',
       '*.js'
     ],
     client: {
